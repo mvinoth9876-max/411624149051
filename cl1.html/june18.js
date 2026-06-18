2bd5a189fe8cf3e3a633623feea2551abb8ad320
unknown <m.vinoth9876@gmail.com>
2026-06-18 10:51:46 +0530

    updated project structure

---
+++ cl1.html/june18.js
@@ -0,0 +1,28 @@
+function example() {
+  console.log("B");
+}
+
+const example2 = () => {
+  console.log("D");
+};
+
+console.log("A");
+
+setTimeout(example, 2000);
+setTimeout(example2, 3000);
+
+console.log("C");
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("hi");
+  }, 1000);
+});
+
+promise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
